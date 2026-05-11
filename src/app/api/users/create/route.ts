448c9fa8c73be6488f16c0d22e83737a1cc44462
fromis _9 <rahadifauzan7@gmail.com>
2026-05-11 09:55:06 +0700

    ref: move pages to protected

--- src/app/api/users/create/route.ts
+++ src/app/api/users/create/route.ts
@@ -10,23 +10,22 @@ export async function POST(req: Request) {
   if (!username) {
     return NextResponse.json(
       { error: "Missing required fields" },
-      { status: 400 }
+      { status: 400 },
     );
   }
 
   try {
-    // Create the room
     const Users = await db.insert(users).values({ username }).returning();
 
     return NextResponse.json(
       { data: Users, messages: "Account has been created" },
-      { status: 201 }
+      { status: 201 },
     );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
       { error: "Failed to create room" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
